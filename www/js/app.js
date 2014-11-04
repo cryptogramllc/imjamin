22e751e28e7e16c96641b8683265d4f10b519cad
Jibran Khan <Jibran@Jibrans-MacBook-Pro.local>
2014-11-04 00:05:57 +0000

    updates

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,7 @@
+
 $(document).ready(function(){
+
+
    $('.col-25').show();
    session_check();
 
@@ -27,9 +30,9 @@ $(document).ready(function(){
            var num_code;
         
            $('input[type="radio"]').each(function(){
-				var checkbox = $(this).is(':checked');
-			    if(checkbox){ num_code = $(this).val(); }
-			});
+				      var checkbox = $(this).is(':checked');
+			        if(checkbox){ num_code = $(this).val(); }
+			     });
 
            var number = $('#mobile').val();
            var mobile = num_code + number; 
@@ -44,69 +47,6 @@ $(document).ready(function(){
 
 			
     });
-
-     $('body').on('change', 'input#avatar', function(){ 
-     		$('form#register').submit();
-     });
-	
-	var options = { 
-	    target: '#output',   // target element(s) to be updated with server response 
-	    beforeSubmit:  beforeSubmit,  // pre-submit callback 
-	    resetForm: false        // reset the form after successful submit 
-	}; 
-
-	$('body').on('submit', 'form#register', function() { 
-	    $(this).ajaxSubmit(options);  //Ajax Submit form            
-	    // return false to prevent standard browser submit and page navigation 
-	    return false; 
-	});
-
-
-
-
-		function beforeSubmit(){
-		//check whether browser fully supports all File API
-		if (window.File && window.FileReader && window.FileList && window.Blob)
-		{
-		    
-		    if( !$('input#avatar').val() ) //check empty input filed
-		    {
-		        $("#output").html("Are you kidding me?");
-		        return false
-		    }
-		    
-		    var fsize = $('#avatar')[0].files[0].size; //get file size
-		    var ftype = $('#avatar')[0].files[0].type; // get file type
-		    
-
-		    //allow only valid image file types 
-		    switch(ftype)
-		    {
-		        case 'image/png': case 'image/gif': case 'image/jpeg': case 'image/pjpeg':
-		            break;
-		        default:
-		            $("#output").html("<b>"+ftype+"</b> Unsupported file type!");
-		            return false
-		    }
-		    
-		    //Allowed file size is less than 1 MB (1048576)
-		    if(fsize>1048576) 
-		    {
-		        $("#output").html("<b>"+ fsize +"</b> Too big Image file! <br />Please reduce the size of your photo using an image editor.");
-		        return false
-		    }
-		            
-		    $('#submit-btn').hide(); //hide submit button
-		    $('#loading-img').show(); //hide submit button
-		    $("#output").html("");  
-		}
-		else
-		{
-		    //Output error to older browsers that do not support HTML5 File API
-		    $("#output").html("Please upgrade your browser, because your current browser lacks some new features we need!");
-		    return false;
-		}
-		}
 
 
 
@@ -144,22 +84,6 @@ $(document).ready(function(){
        }); 
 
    }
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- www/js/index.js
+++ www/js/index.js
@@ -1,52 +1,56 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//Camera Section
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+// PhoneGap is ready
+function onDeviceReady() {}
 
-        console.log('Received Event: ' + id);
+// Called when capture operation is finished
+//
+function captureSuccess(mediaFiles) {    
+    uploadFile(mediaFiles[0]);
+}
 
+// Called if something bad happens.
+//
+function captureError(error) {
+    var msg = 'An error occurred during capture: ' + error.code;
+    navigator.notification.alert(msg, null, 'Uh oh!');
+}
 
-           $.mobile.allowCrossDomainPages = true;
-    }
-};
+// A button will call this function
+//
+function captureImage() {
+    // Launch device camera application,
+    // allowing user to capture only one image by {limit: 1}
+    navigator.device.capture.captureImage(captureSuccess, captureError, { limit: 1 });
+}
+
+// Upload files to server
+function uploadFile(mediaFile) {
+    path = mediaFile.fullPath;
+    name = mediaFile.name;
+    
+    var options = new FileUploadOptions();
+    options.fileKey="file";
+    options.fileName=mediaFile.name;
+    options.mimeType="image/jpeg";
+
+    var params = new Object();
+    params.fullpath = path;
+    params.name = name;
+
+    options.params = params;
+    options.chunkedMode = false;
+    
+    var ft = new FileTransfer();
+    ft.upload( path, "http://www.serverurl.com/image_upload",
+        function(result) {
+            //upload successful            
+        },
+        function(error) {
+            //upload unsuccessful, error occured while upload. 
+        },
+        options
+        );
+}

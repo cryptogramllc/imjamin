a2cecf65222e69228c103cd113b30fe3d994bb61
EC2 Default User <ec2-user@ip-172-31-26-46.us-west-2.compute.internal>
2015-04-26 18:26:18 +0000

    updates

---
+++ www/trimMp3.php
@@ -0,0 +1,15 @@
+<?php 
+
+// if you want to get more examples or the class with comments, please download this zip file: 
+// http://www.mfman.net/class.mp3.zip 
+  echo gmdate("H:i:s", 194);
+  
+
+  echo gmdate("H:i:s", 214);
+  
+  /*
+
+             shell_exec("ffmpeg -i /var/www/html/media/01-All-My-Days.mp4 -ss 00:00:03 -t 00:00:08 -async 1 -strict -2 /var/www/html/media/01-All-My-Days-trim.mp4");
+*/
+
+?>
